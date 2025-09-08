d8d58c7bc3ca3187260ab59c4b59fb845c4efe1f
umbara-web <umbara81.web@gmail.com>
2025-09-08 09:01:21 +0700

    tugas:exercise-2.js

--- modul-01/exercise-2.js
+++ modul-01/exercise-2.js
@@ -22,3 +22,39 @@ function cekGanjilGenapTernary(bilangan) {
 
 console.log(cekGanjilGenapTernary(25)); // Output: "Bilangan ganjil"
 console.log(cekGanjilGenapTernary(2)); // Output: "Bilangan genap"
+
+// No.2
+// Tulis kode untuk memeriksa apakah bilangan tersebut bilangan prima atau bukan
+// Contoh: 7 → 7 adalah bilangan prima
+// Contoh: 6 → 6 bukan bilangan prima
+function bilPrima(angka) {
+  // Bilangan kurang dari atau sama dengan 1 bukanlah bilangan prima
+  if (angka <= 1) {
+    return 'bukan bilangan prima';
+  }
+
+  // Bilangan 2 adalah bilangan prima
+  if (angka === 2) {
+    return 'bilangan prima';
+  }
+
+  // Jika bilangan genap selain 2, maka bukan prima
+  if (angka % 2 === 0) {
+    return 'bukan bilangan prima';
+  }
+
+  // Cek pembagi dari 3 hingga akar kuadrat dari angka tersebut
+  // Hanya perlu memeriksa bilangan ganjil
+  for (let i = 3; i <= Math.sqrt(angka); i += 2) {
+    if (angka % i === 0) {
+      return 'bukan bilangan prima'; // Ditemukan pembagi, jadi bukan prima
+    }
+  }
+
+  // Jika tidak ditemukan pembagi, maka bilangan tersebut adalah prima
+  return 'bilangan prima';
+}
+
+// Contoh penggunaan
+console.log(bilPrima(7)); //Output : bilangan prima
+console.log(bilPrima(6)); //Output : bukan bilangan prima
