9cbd119f2d486cca936490a6d1e77c5f21ee93d3
umbara-web <umbara81.web@gmail.com>
2025-10-11 11:14:22 +0700

    feat: header, footer, hero, integration, feature

--- modul-02/exercise-10/my-app/src/app/page.tsx
+++ modul-02/exercise-10/my-app/src/app/page.tsx
@@ -1,14 +1,26 @@
-import Header from './header/page';
-import Footer from './footer/page';
-import HeroSection from './hero-section/page';
-import CardArticles from './card-articles/page';
+// import Header from './header/page';
+// import Footer from './footer/page';
+// import HeroSection from './hero-section/page';
+
+// import { HeroHeader } from '@/components/header';
+import Features from '@/components/features-4';
+import FooterSection from '@/components/footer';
+import HeroSection from '@/components/hero-section';
+import IntegrationsSection from '@/components/integrations-7';
+
+// import CardArticles from './card-articles/page';
 export default function Home() {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
+      {/* <HeroSection /> */}
+      {/* <CardArticles /> */}
+      {/* <Footer /> */}
+      {/* <HeroHeader /> */}
       <HeroSection />
-      <CardArticles />
-      <Footer />
+      <Features />
+      <IntegrationsSection />
+      <FooterSection />
     </>
   );
 }
---
+++ modul-02/exercise-10/my-app/src/components/DarkModeBtn.tsx
@@ -0,0 +1,38 @@
+import { Sun, Moon } from 'lucide-react';
+import useTheme from '../contexts/theme';
+
+const DarkModeBtn = () => {
+  const { darkMode, toggleDarkMode } = useTheme();
+  return (
+    <div className='flex items-center gap-3'>
+      <div className='flex w-full justify-end-safe items-center text-muted-foreground font-semibold text-sm lg:hidden'>
+        {darkMode ? 'Light Mode' : 'Dark Mode'}
+      </div>
+      <button
+        onClick={toggleDarkMode}
+        className={`relative flex items-center p-1 rounded-full border border-gray-300 dark:border-gray-700 shadow-xl hover:cursor-pointer transition-all duration-300 bg-gray-200 hover:bg-gray-300 dark:bg-gray-800 dark:hover:bg-gray-600 ${
+          darkMode ? 'bg-gray-800' : ''
+        }`}
+      >
+        <div
+          className={`absolute top-1/2 -translate-y-1/2 bg-white p-0.5 rounded-full flex items-center justify-center transition-all duration-500 ${
+            darkMode ? 'translate-x-5.5' : 'translate-x-0'
+          }`}
+        >
+          {darkMode ? (
+            <Moon size={18} className='text-black' />
+          ) : (
+            <Sun size={18} className='text-black' />
+          )}
+        </div>
+
+        <div className='flex w-full justify-between items-center gap-3 text-white '>
+          <Sun size={16} />
+          <Moon size={16} className='text-black dark:text-white' />
+        </div>
+      </button>
+    </div>
+  );
+};
+
+export default DarkModeBtn;
